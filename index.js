6f9c77ed42864208e7623267e5b318b10e9ba21d
Aris Vayas <aris.vayas@gmail.com>
2022-04-05 13:30:21 -0400

    commit tree 40629a2948043e9636f9d0a2af04cf02c61ff151

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(x){
+     cats.push(x)
+}
+function destructivelyPrependCat(n){
+    cats.unshift(n)
+}
+function  destructivelyRemoveLastCat(n){
+    cats.pop(n)
+}
+function destructivelyRemoveFirstCat(n){
+    cats.shift(n)
+}
+function appendCat(n){
+    const appendCat=[...cats, n]
+return appendCat
+}
+function prependCat(n){
+const prependCat = [n, ...cats]
+return prependCat
+}
+function removeLastCat(){
+    const cats =["Milo", "Otis", "Garfield"]
+   const cat= cats.slice(0, cats.length - 1)
+    return cat
+}
+function removeFirstCat(){
+    const cats =["Milo", "Otis", "Garfield"]
+   const cat= cats.slice(1)
+    return cat
+}
